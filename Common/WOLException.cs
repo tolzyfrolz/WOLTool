17b609acc1c7d534da60bf4450cd98b35edec8cb
tolzyfrolz <200447758+tolzyfrolz@users.noreply.github.com>
2025-05-02 12:38:43 -0500

    Add project files.

---
+++ Common/WOLException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace WOLTool.Common
+{
+    public sealed class WOLException : Exception
+    {
+        public WOLException()
+        {
+        }
+
+        public WOLException(string message)
+            : base(message)
+        {
+        }
+
+        public WOLException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
---
+++ Sockets/WOLSocket.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Net;
+using System.Net.NetworkInformation;
+using System;
+using System.Threading.Tasks;
+using WOLTool.Common;
+using System.Linq;
+
+namespace WOLTool.Sockets
+{
+    /// <summary>
+    /// A class that provides methods for sending Wake-on-LAN (WOL) magic packets to wake up computers on a network.
+    /// Encapsulates a Socket object for sending UDP packets.
+    /// </summary>
+    public sealed class WOLSocket : IDisposable
+    {
+        private readonly Socket _socket;
+        private static readonly IReadOnlyList<IPEndPoint> _endpoints = new List<IPEndPoint>
+        {
+            new IPEndPoint(IPAddress.Broadcast, 7), // echo
+            new IPEndPoint(IPAddress.Broadcast, 9) // discard
+        };
+
+        public WOLSocket()
+        {
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp)
+            {
+                EnableBroadcast = true // Enable broadcast, required for macOS compatibility
+            };
+        }
+
+        /// <summary>
+        /// Broadcasts a Wake-on-LAN magic packet to the specified MAC addresses.
+        /// </summary>
+        /// <param name="macAddresses">MAC Addresses to wake in string format (will be parsed).</param>
+        public void Broadcast(IEnumerable<string> macAddresses)
+        {
+            var physicalAddresses = macAddresses
+                .Select(mac => PhysicalAddress.Parse(mac));
+            Broadcast(physicalAddresses);
+        }
+
+        /// <summary>
+        /// Asynchronously Broadcasts a Wake-on-LAN magic packet to the specified MAC addresses.
+        /// </summary>
+        /// <param name="macAddresses">MAC Addresses to wake in string format (will be parsed).</param>
+        /// <returns>Task</returns>
+        public async Task BroadcastAsync(IEnumerable<string> macAddresses)
+        {
+            var physicalAddresses = macAddresses
+                .Select(mac => PhysicalAddress.Parse(mac));
+            await BroadcastAsync(physicalAddresses);
+        }
+
+        /// <summary>
+        /// Broadcasts a Wake-on-LAN magic packet to the specified MAC address.
+        /// </summary>
+        /// <param name="macAddress">MAC Address to wake in string format (will be parsed).</param>
+        public void Broadcast(string macAddress)
+        {
+            var physicalAddress = PhysicalAddress.Parse(macAddress);
+            Broadcast(physicalAddress);
+        }
+
+        /// <summary>
+        /// Asynchronously Broadcasts a Wake-on-LAN magic packet to the specified MAC address.
+        /// </summary>
+        /// <param name="macAddress">MAC Address to wake in string format (will be parsed).</param>
+        /// <returns>Task</returns>
+        public async Task BroadcastAsync(string macAddress)
+        {
+            var physicalAddress = PhysicalAddress.Parse(macAddress);
+            await BroadcastAsync(physicalAddress);
+        }
+
+        /// <summary>
+        /// Broadcasts a Wake-on-LAN magic packet to the specified MAC address.
+        /// </summary>
+        /// <param name="macAddress">MAC Address to wake.</param>
+        public void Broadcast(PhysicalAddress macAddress) =>
+            Broadcast_Internal(macAddress);
+
+        /// <summary>
+        /// Broadcasts a Wake-on-LAN magic packet to the specified MAC addresses.
+        /// </summary>
+        /// <param name="macAddresses">MAC Addresses to wake.</param>
+        public void Broadcast(IEnumerable<PhysicalAddress> macAddresses)
+        {
+            foreach (var macAddress in macAddresses)
+            {
+                Broadcast_Internal(macAddress);
+            }
+        }
+
+        /// <summary>
+        /// Asynchronously Broadcasts a Wake-on-LAN magic packet to the specified MAC address.
+        /// </summary>
+        /// <param name="macAddress">MAC Address to wake.</param>
+        /// <returns>Task</returns>
+        public async Task BroadcastAsync(PhysicalAddress macAddress)
+        {
+            await Task.Run(() =>
+            {
+                Broadcast(macAddress);
+            });
+        }
+
+        /// <summary>
+        /// Asynchronously Broadcasts a Wake-on-LAN magic packet to the specified MAC addresses.
+        /// </summary>
+        /// <param name="macAddresses">MAC Addresses to wake.</param>
+        /// <returns>Task</returns>
+        public async Task BroadcastAsync(IEnumerable<PhysicalAddress> macAddresses)
+        {
+            await Task.Run(() =>
+            {
+                Broadcast(macAddresses);
+            });
+        }
+
+        private void Broadcast_Internal(PhysicalAddress mac)
+        {
+            try
+            {
+                byte[] magicPacket = BuildMagicPacket(mac); // Get magic packet byte array based on MAC Address
+                foreach (var ep in _endpoints) // Broadcast to *all* WOL Endpoints
+                {
+                    _socket.SendTo(magicPacket, ep); // Broadcast magic packet
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new WOLException("ERROR broadcasting WakeOnLan Magic Packet.", ex);
+            }
+        }
+
+        private static byte[] BuildMagicPacket(PhysicalAddress macAddress)
+        {
+            byte[] macBytes = macAddress.GetAddressBytes(); // Convert 48 bit MAC Address to array of bytes
+            byte[] magicPacket = new byte[102];
+            for (int i = 0; i < 6; i++) // 6 times 0xFF
+            {
+                magicPacket[i] = 0xFF;
+            }
+            for (int i = 6; i < 102; i += 6) // 16 times MAC Address
+            {
+                Buffer.BlockCopy(macBytes, 0, magicPacket, i, 6);
+            }
+            return magicPacket; // 102 Byte Magic Packet
+        }
+
+        public void Dispose()
+        {
+            _socket.Dispose();
+        }
+    }
+}
