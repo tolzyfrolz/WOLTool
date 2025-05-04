dcffc97cd344e8afce8979faecef1e1856936211
tolzyfrolz <200447758+tolzyfrolz@users.noreply.github.com>
2025-05-04 14:28:01 -0500

    Revamp tests

--- WOLTool_Tests/Program.cs
+++ WOLTool_Tests/Program.cs
@@ -7,6 +7,7 @@ namespace WOLTool_Tests
     {
         static async Task<int> Main(string[] args)
         {
+            Console.WriteLine($"Running Tests for DotNet {Environment.Version}");
             const string macStr1 = "84:1B:77:4C:9F:22";
             const string macStr2 = "3C:98:23:11:A6:7B";
             const string macStr3 = "00:15:5D:8E:44:C1";
@@ -15,144 +16,43 @@ namespace WOLTool_Tests
             var mac3 = PhysicalAddress.Parse(macStr3);
             var macStrList = new List<string> { macStr1, macStr2, macStr3 };
             var macList = new List<PhysicalAddress> { mac1, mac2, mac3 };
-            if (Test_Broadcast(macStr1) &
-                Test_Broadcast(mac1) &
-                Test_Broadcast(macStrList) &
-                Test_Broadcast(macList) &
-                await Test_BroadcastAsync(macStr1) &
-                await Test_BroadcastAsync(mac1) &
-                await Test_BroadcastAsync(macStrList) &
-                await Test_BroadcastAsync(macList))
-            {
-                Console.WriteLine("All tests passed.");
-            }
-            else
-            {
-                Console.WriteLine("Some tests failed.");
-            }
+            var t1 = Test_BroadcastAsync(macStr1);
+            var t2 = Test_BroadcastAsync(mac1);
+            var t3 = Test_BroadcastAsync(macStrList);
+            var t4 = Test_BroadcastAsync(macList);
+            Test_Broadcast(macStr1);
+            Test_Broadcast(mac1);
+            Test_Broadcast(macStrList);
+            Test_Broadcast(macList);
+            await Task.WhenAll(t1, t2, t3, t4);
+            Console.WriteLine("All tests passed.");
             return 0;
         }
 
 
-        private static bool Test_Broadcast(IEnumerable<string> macAddresses)
-        {
-            Console.WriteLine("Test_Broadcast(IEnumerable<string> macAddresses)");
-            try
-            {
-                WakeOnLan.Broadcast(macAddresses);
-                return true;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-                return false;
-            }
-        }
+        private static void Test_Broadcast(IEnumerable<string> macAddresses) =>
+            WakeOnLan.Broadcast(macAddresses);
 
-        private static async Task<bool> Test_BroadcastAsync(IEnumerable<string> macAddresses)
-        {
-            Console.WriteLine("Test_BroadcastAsync(IEnumerable<string> macAddresses)");
-            try
-            {
-                await WakeOnLan.BroadcastAsync(macAddresses);
-                return true;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-                return false;
-            }
-        }
+        private static async Task Test_BroadcastAsync(IEnumerable<string> macAddresses) =>
+            await WakeOnLan.BroadcastAsync(macAddresses);
 
-        private static bool Test_Broadcast(string macAddress)
-        {
-            Console.WriteLine("Test_Broadcast(string macAddress)");
-            try
-            {
-                WakeOnLan.Broadcast(macAddress);
-                return true;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-                return false;
-            }
-        }
+        private static void Test_Broadcast(string macAddress) =>
+            WakeOnLan.Broadcast(macAddress);
 
-        private static async Task<bool> Test_BroadcastAsync(string macAddress)
-        {
-            Console.WriteLine("Test_BroadcastAsync(string macAddress)");
-            try
-            {
-                await WakeOnLan.BroadcastAsync(macAddress);
-                return true;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-                return false;
-            }
-        }
+        private static async Task Test_BroadcastAsync(string macAddress) =>
+            await WakeOnLan.BroadcastAsync(macAddress);
 
-        private static bool Test_Broadcast(PhysicalAddress macAddress)
-        {
-            Console.WriteLine("Test_Broadcast(PhysicalAddress macAddress)");
-            try
-            {
-                WakeOnLan.Broadcast(macAddress);
-                return true;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-                return false;
-            }
-        }
+        private static void Test_Broadcast(PhysicalAddress macAddress) =>
+            WakeOnLan.Broadcast(macAddress);
 
-        private static bool Test_Broadcast(IEnumerable<PhysicalAddress> macAddresses)
-        {
-            Console.WriteLine("Test_Broadcast(IEnumerable<PhysicalAddress> macAddresses)");
-            try
-            {
-                WakeOnLan.Broadcast(macAddresses);
-                return true;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-                return false;
-            }
-        }
+        private static void Test_Broadcast(IEnumerable<PhysicalAddress> macAddresses) =>
+            WakeOnLan.Broadcast(macAddresses);
 
-        private static async Task<bool> Test_BroadcastAsync(PhysicalAddress macAddress)
-        {
-            Console.WriteLine("Test_BroadcastAsync(PhysicalAddress macAddress)");
-            try
-            {
-                await WakeOnLan.BroadcastAsync(macAddress);
-                return true;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-                return false;
-            }
-        }
+        private static async Task Test_BroadcastAsync(PhysicalAddress macAddress) =>
+            await WakeOnLan.BroadcastAsync(macAddress);
 
-        private static async Task<bool> Test_BroadcastAsync(IEnumerable<PhysicalAddress> macAddresses)
-        {
-            Console.WriteLine("Test_BroadcastAsync(IEnumerable<PhysicalAddress> macAddresses)");
-            try
-            {
-                await WakeOnLan.BroadcastAsync(macAddresses);
-                return true;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-                return false;
-            }
-        }
+        private static async Task Test_BroadcastAsync(IEnumerable<PhysicalAddress> macAddresses) =>
+            await WakeOnLan.BroadcastAsync(macAddresses);
     }
 }
 
